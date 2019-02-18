606323d3e06f0b76dc53bf9802f55ee56c5975af
Vikas <Khamat.bn@gmail.com>
2019-02-18 18:17:25 +0530

    Initial commit

---
+++ app/src/main/java/com/Vikas/hambutton/MainActivity.java
@@ -0,0 +1,75 @@
+package com.Vikas.hambutton;
+
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+import com.nightonke.boommenu.BoomMenuButton;
+import com.nightonke.boommenu.Types.BoomType;
+import com.nightonke.boommenu.Types.ButtonType;
+import com.nightonke.boommenu.Types.PlaceType;
+import com.nightonke.boommenu.Util;
+
+public class MainActivity extends AppCompatActivity {
+
+    private boolean init = false;
+    private BoomMenuButton boomMenuButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        boomMenuButton = findViewById(R.id.boom);
+
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+
+        // Use a param to record whether the boom button has been initialized
+        // Because we don't need to init it again when onResume()
+        if (init) return;
+        init = true;
+
+        Drawable[] subButtonDrawables = new Drawable[3];
+        int[] drawablesResource = new int[]{
+                R.drawable.search,
+                R.drawable.refresh,
+                R.drawable.settings
+        };
+        for (int i = 0; i < 3; i++)
+            subButtonDrawables[i] = ContextCompat.getDrawable(this, drawablesResource[i]);
+
+        int[][] subButtonColors = new int[3][2];
+        for (int i = 0; i < 3; i++) {
+            subButtonColors[i][1] = ContextCompat.getColor(this, R.color.textcolor);
+            subButtonColors[i][0] = Util.getInstance().getPressedColor(subButtonColors[i][1]);
+        }
+
+        // Now with Builder, you can init BMB more convenient
+        new BoomMenuButton.Builder()
+                .addSubButton(ContextCompat.getDrawable(this, R.drawable.search), subButtonColors[0], "Search")
+                .addSubButton(ContextCompat.getDrawable(this, R.drawable.refresh), subButtonColors[0], "Refresh")
+                .addSubButton(ContextCompat.getDrawable(this, R.drawable.settings), subButtonColors[0], "Setting")
+                .button(ButtonType.HAM)
+                .boom(BoomType.PARABOLA)
+                .place(PlaceType.HAM_3_1)
+                .subButtonTextColor(ContextCompat.getColor(this, R.color.colorPrimaryDark))
+                .subButtonsShadow(Util.getInstance().dp2px(2), Util.getInstance().dp2px(2))
+                .onSubButtonClick(new BoomMenuButton.OnSubButtonClickListener() {
+                    @Override
+                    public void onClick(int buttonIndex) {
+                        switch (buttonIndex) {
+                            default:
+                                Toast.makeText(getApplicationContext(), "Ham Button Clicked", Toast.LENGTH_SHORT).show();
+                                break;
+                        }
+                    }
+                })
+                .init(boomMenuButton);
+    }
+}
